5ba266afb186570c8b694a3dd8ebf7f5df317ea4
Rjegan2 <ryanegan212002@gmail.com>
2023-09-07 19:49:25 -0500

    Variables for Player Movement/Actions
    Added variables for player movement/actions in PlayerController

--- Illuminate/Assets/Scripts/PlayerController.cs
+++ Illuminate/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private PlayerInput playerInput;
+
+
+    // actions that will be done by the player
+    private InputAction move;
+    private InputAction jump;
+    private InputAction death;
+
+    // variable to check if player is moving or not
+    private bool isPlayerMoving;
+    // reference for this boolean:
+    [SerializeField] private GameObject player;
+
+    // variable for how fast the player will move
+    [SerializeField] private float playerSpeed;
+
+    // direction for player
+    private float moveDirection;
+
     // Start is called before the first frame update
     void Start()
     {
