06a5487d71432e40b1fa447c4bfd70f751609b7d
Rjegan2 <ryanegan212002@gmail.com>
2023-09-07 20:08:35 -0500

    EnableInputs() for PlayerController
    EnableInputs() function created in PlayerController to enable and read in movement inputs

--- Illuminate/Assets/Scripts/PlayerController.cs
+++ Illuminate/Assets/Scripts/PlayerController.cs
@@ -27,12 +27,45 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        EnableInputs();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(isPlayerMoving)
+        {
+            moveDirection = move.ReadValue<float>();
+        }
+    }
+
+    // function that enables action inputs to be read
+    private void EnableInputs()
+    {
+        // Activating action map
+        playerInput.currentActionMap.Enable();
+
+        // reading in inputs
+        move = playerInput.currentActionMap.FindAction("Move");
+        jump = playerInput.currentActionMap.FindAction("Jump");
+
+        move.started += Move_started;
+        move.canceled += Move_canceled;
+        jump.started += Jump_started;
+    }
+
+    private void Jump_started(InputAction.CallbackContext obj)
+    {
         
+    }
+
+    private void Move_canceled(InputAction.CallbackContext obj)
+    {
+        isPlayerMoving = false;
+    }
+
+    private void Move_started(InputAction.CallbackContext obj)
+    {
+        isPlayerMoving = true;
     }
 }
