716e765b31467d721d2e78229577be8f31677f6b
Dylan246 <99310919+Dylan246@users.noreply.github.com>
2023-09-23 16:48:17 -0500

    Modified platform scripts
    Added variables to easily adjust speed/distance for each moving platform object individually

--- Illuminate/Assets/Scripts/MovePlatform.cs
+++ Illuminate/Assets/Scripts/MovePlatform.cs
@@ -9,23 +9,25 @@ public class MovePlatform : MonoBehaviour
     [SerializeField] private GameObject movingPlatform;
     private float originalXPos;
     private bool goLeft;
+    public int distance; //Distance will be defined in each object so that it can be varied (baseline value 3)
+    public int speed; //Also defined in each object so it can be varied (baseline value 1f)
 
 
     // Start is called before the first frame update
     void Start()
     {
-        originalXPos = movingPlatform.transform.position.x; 
+        originalXPos = movingPlatform.transform.position.x; //Save the original position
         goLeft = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (movingPlatform.transform.position.x >= originalXPos + 3) //Moves 6 units in total 
+        if (movingPlatform.transform.position.x >= originalXPos + distance) //Moves distance * 2 units in total 
         {
             goLeft = true;
         }
-        if (movingPlatform.transform.position.x <= originalXPos - 3)
+        if (movingPlatform.transform.position.x <= originalXPos - distance)
         {
             goLeft = false;
         }
@@ -34,14 +36,14 @@ public class MovePlatform : MonoBehaviour
             print("go left");
             //print(gameObject.transform.position.x);
             //print(originalXPos);
-            movingPlatform.transform.Translate(new Vector3(-1f, 0) * Time.deltaTime, Space.World);
+            movingPlatform.transform.Translate(new Vector3(-speed, 0) * Time.deltaTime, Space.World);
         }
         if (goLeft == false) // Then go right
         { 
             print("go right");
             //print(gameObject.transform.position.x);
             //print(originalXPos);
-            movingPlatform.transform.Translate(new Vector3(1f, 0) * Time.deltaTime, Space.World);
+            movingPlatform.transform.Translate(new Vector3(speed, 0) * Time.deltaTime, Space.World);
         }
     }
 }
--- Illuminate/Assets/Scripts/RotatePlatform.cs
+++ Illuminate/Assets/Scripts/RotatePlatform.cs
@@ -7,18 +7,18 @@ using static UnityEngine.Rendering.VirtualTexturing.Debugging;
 public class RotatePlatform : MonoBehaviour
 {
     [SerializeField] private GameObject rotatingPlatform;
-
+    public int rotationalSpeed; //Defined in each object so that it can vary (baseline value 25f)
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //Not being used as of now
     }
 
     // Update is called once per frame
     void Update()
     {
-       //Simple for now
-       rotatingPlatform.transform.Rotate(new Vector3(0, 0, 25f) * Time.deltaTime, Space.World);
+       //Simple for now, just rotate at a constant speed 
+       rotatingPlatform.transform.Rotate(new Vector3(0, 0, rotationalSpeed) * Time.deltaTime, Space.World);
     }
 }
