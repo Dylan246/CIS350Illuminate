4c960e87918ffe69a0e6edb2abc1193e535edb3f
SamuelSD7 <sdwyer1313@icloud.com>
2023-09-27 23:18:41 -0500

    start on new level

--- Illuminate/Assets/Scripts/MovePlatform.cs
+++ Illuminate/Assets/Scripts/MovePlatform.cs
@@ -1,49 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static UnityEngine.Rendering.VirtualTexturing.Debugging;
 
 public class MovePlatform : MonoBehaviour
 {
     [SerializeField] private GameObject movingPlatform;
+    public bool MoveUpDown;
     private float originalXPos;
     private bool goLeft;
-    public int distance; //Distance will be defined in each object so that it can be varied (baseline value 3)
+    public float distance; //Distance will be defined in each object so that it can be varied (baseline value 3)
     public int speed; //Also defined in each object so it can be varied (baseline value 1f)
 
 
     // Start is called before the first frame update
     void Start()
     {
-        originalXPos = movingPlatform.transform.position.x; //Save the original position
+        if(!MoveUpDown)
+        {
+            originalXPos = movingPlatform.transform.position.x; //Save the original position
+        }
+        else
+        {
+            originalXPos = movingPlatform.transform.position.y; //Save the original position
+        }
         goLeft = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (movingPlatform.transform.position.x >= originalXPos + distance) //Moves distance * 2 units in total 
+        if(!MoveUpDown)
         {
-            goLeft = true;
+            if (movingPlatform.transform.position.x >= originalXPos + distance) //Moves distance * 2 units in total 
+            {
+                goLeft = true;
+            }
+            if (movingPlatform.transform.position.x <= originalXPos - distance)
+            {
+                goLeft = false;
+            }
+            if (goLeft == true)
+            {
+                //print(gameObject.transform.position.x);
+                //print(originalXPos);
+                movingPlatform.transform.Translate(new Vector3(-speed, 0) * Time.deltaTime, Space.World);
+            }
+            if (goLeft == false) // Then go right
+            {
+                //print(gameObject.transform.position.x);
+                //print(originalXPos);
+                movingPlatform.transform.Translate(new Vector3(speed, 0) * Time.deltaTime, Space.World);
+            }
         }
-        if (movingPlatform.transform.position.x <= originalXPos - distance)
+        else
         {
-            goLeft = false;
+            if (movingPlatform.transform.position.y >= originalXPos + distance) //Moves distance * 2 units in total 
+            {
+                goLeft = true;
+            }
+            if (movingPlatform.transform.position.y <= originalXPos - distance)
+            {
+                goLeft = false;
+            }
+            if (goLeft == true)
+            {
+                //print(gameObject.transform.position.x);
+                //print(originalXPos);
+                movingPlatform.transform.Translate(new Vector3(0, -speed) * Time.deltaTime, Space.World);
+            }
+            if (goLeft == false) // Then go right
+            {
+                //print(gameObject.transform.position.x);
+                //print(originalXPos);
+                movingPlatform.transform.Translate(new Vector3(0, speed) * Time.deltaTime, Space.World);
+            }
         }
-        if (goLeft == true)
-        {
-            print("go left");
-            //print(gameObject.transform.position.x);
-            //print(originalXPos);
-            movingPlatform.transform.Translate(new Vector3(-speed, 0) * Time.deltaTime, Space.World);
-        }
-        if (goLeft == false) // Then go right
-        { 
-            print("go right");
-            //print(gameObject.transform.position.x);
-            //print(originalXPos);
-            movingPlatform.transform.Translate(new Vector3(speed, 0) * Time.deltaTime, Space.World);
-        }
+        
     }
 }
